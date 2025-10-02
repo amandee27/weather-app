62ac8b41894ac2c73907dc64b53fed8c21a5c93d
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-10-02 23:22:48 +0100

    Implement all dropdowns in the weather app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import logo from "./assets/icons/logo.svg";
 import unitsIcon from "./assets/icons/icon-units.svg";
 import dropdownIcon from "./assets/icons/icon-dropdown.svg";
+import checkmarkIcon from "./assets/icons/icon-checkmark.svg";
 import searchIcon from "./assets/icons/icon-search.svg";
 import bgImage from "./assets/icons/bg-today-large.svg";
 import sunnyIcon from "./assets/icons/icon-sunny.webp";
@@ -15,8 +16,26 @@ import rainIcon from "./assets/icons/icon-rain.webp";
 import snowIcon from "./assets/icons/icon-snow.webp";
 import stormIcon from "./assets/icons/icon-storm.webp";
 
+const options = ["London", "Wembley", "Maidenhead", "Bath"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"];
+
 function App() {
   const [count, setCount] = useState(0);
+  const [country, setCountry] = useState("");
+  const [day, setDay] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDaysOpen, setIsDaysOpen] = useState(false);
+  const city = "TTTT";
+  const [selectedUnits, setSelectedUnits] = useState({
+    temperature: "Celcius (°C)",
+    "wind speed": "km/h",
+    precipitation: "Millimeters(mm)",
+  });
+  const [isUnitOpen, setIsUnitOpen] = useState(false);
+
+  const toggleSelect = (category: string, option: string) => {
+    setSelectedUnits((prev) => ({ ...prev, [category]: option }));
+  };
 
   return (
     <div
@@ -28,30 +47,125 @@ function App() {
           <div>
             <img src={logo} alt="Logo" />
           </div>
-          <div>
-            <button className="flex gap-[10px] items-center text-white bg-gray-600 w-[119px] h-[19px] px-[16px] py-[12px] rounded-md">
+          <div className="relative ">
+            <button
+              onClick={() => setIsUnitOpen(!isUnitOpen)}
+              className="flex gap-[10px] items-center text-white bg-gray-600 w-[119px] h-[19px] px-[16px] py-[12px] rounded-md"
+            >
               <img className="w-[16px] h-[16px]" src={unitsIcon} />
               <span className="text-md">Units</span>
               <img className="w-[12px] h-[18px]" src={dropdownIcon} />
             </button>
+            {isUnitOpen && (
+              <div className="w-[198px] absolute right-0 top-full mt-2 z-10">
+                <ul className="p-2 bg-[var(--color-forcast-hoyrlybg)] rounded-md border-1 border-[var(--color-color-stroke)]  ">
+                  <li className="py-1  font-bold text-[var(--color-text-subheading)] text-base text-left">
+                    Switch To Imperial
+                  </li>
+                  <li className="w-full p-2 text-[var(--color-text-subheading)] font-bold text-sm text-left">
+                    Temperature
+                  </li>
+                  {["Celcius (°C)", "Fahrenheit (°F)"].map((option) => (
+                    <li
+                      key={option}
+                      onClick={() => {
+                        toggleSelect("temperature", option);
+                      }}
+                      className="flex  justify-between text-[var(--color-text)] hover:bg-[var(--color-color-stroke)] rounded-md p-2"
+                    >
+                      {option}{" "}
+                      {option === selectedUnits.temperature && (
+                        <img
+                          className="w-[12px] h-[18px]"
+                          src={checkmarkIcon}
+                        />
+                      )}
+                    </li>
+                  ))}
+                  <li className="border-1 border-t border-[var(--color-color-stroke)] my-1" />
+                  <li className="w-full p-2 text-[var(--color-text-subheading)] font-bold text-sm text-left">
+                    Wind Speed
+                  </li>
+                  {["km/h", "mph"].map((option) => (
+                    <li
+                      key={option}
+                      onClick={() => {
+                        toggleSelect("wind speed", option);
+                      }}
+                      className="flex  justify-between text-[var(--color-text)] hover:bg-[var(--color-color-stroke)] rounded-md p-2"
+                    >
+                      {option}
+                      {option === selectedUnits["wind speed"] && (
+                        <img
+                          className="w-[12px] h-[18px]"
+                          src={checkmarkIcon}
+                        />
+                      )}
+                    </li>
+                  ))}
+                  <li className="border-1 border-t border-[var(--color-color-stroke)] my-1" />
+                  <li className="w-full p-2 text-[var(--color-text-subheading)] font-bold text-sm text-left">
+                    Precipitation
+                  </li>
+                  {["Millimeters(mm)", "Inches(in)"].map((option) => (
+                    <li
+                      key={option}
+                      onClick={() => {
+                        toggleSelect("precipitation", option);
+                      }}
+                      className="flex  justify-between text-[var(--color-text)] hover:bg-[var(--color-color-stroke)] rounded-md p-2"
+                    >
+                      {option}
+                      {option === selectedUnits["precipitation"] && (
+                        <img
+                          className="w-[12px] h-[18px]"
+                          src={checkmarkIcon}
+                        />
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </nav>
       </header>
       <h1 className="text-white text-6xl">How's the sky looking today?</h1>
       <div className="px-20 py-10 w-[1440px] h-full ">
         <div className="flex justify-center mb-8">
-          <div className="relative w-[656px] h-[56px] flex items-center ">
-            <img
-              className="absolute left-5 top-1/2 transform -translate-y-1/2 w-[16px] h-[16px]"
-              src={searchIcon}
-            ></img>
-            <input
-              type="text"
-              id="search-bar"
-              placeholder="Search for a place..."
-              className="bg-[var(--color-bg-search-box)] ps-20 text-[var(--color-text)] w-[526px] h-[56px] rounded-lg"
-            />
+          <div className="flex items-center">
+            <div className="relative w-[526px] h-[56px]">
+              <img
+                className="absolute left-5 top-1/2 transform -translate-y-1/2 w-[16px] h-[16px]"
+                src={searchIcon}
+              ></img>
+              <input
+                value={country}
+                onFocus={() => setIsOpen(true)}
+                onBlur={() => setTimeout(() => setIsOpen(false), 150)}
+                type="text"
+                id="search-bar"
+                placeholder="Search for a place..."
+                className="bg-[var(--color-bg-search-box)] ps-20 text-[var(--color-text)] w-[526px] h-[56px] rounded-lg"
+              />
 
+              {isOpen && (
+                <ul className="absolute mt-2 w-full bg-[var(--color-bg-search-box)] rounded-lg shadow-lg ">
+                  {options.map((city, index) => (
+                    <li
+                      key={index}
+                      onClick={() => {
+                        setCountry(city);
+                        setIsOpen(false);
+                      }}
+                      className="p-3 hover:bg-[var(--color-color-stroke)] cursor-pointer text-[var(--color-text)] text-left"
+                    >
+                      {city}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
             <button className="bg-[var(--color-search-button)] text-[var(--color-text)] rounded-lg mx-5 px-5 py-2">
               Search
             </button>
@@ -226,11 +340,35 @@ function App() {
                 <div>
                   <h3 className="text-white text-left">Hourly forcast</h3>
                 </div>
-                <div className="justify-items-end">
-                  <button className="flex gap-[10px] items-center justify-center text-white bg-[var(--color-color-stroke)] w-[129px] h-[37px] rounded-md">
+                <div className=" relative justify-items-end w-[129px]">
+                  <button
+                    onClick={() => {
+                      setIsDaysOpen(true);
+                    }}
+                    onBlur={() => {
+                      setIsDaysOpen(false);
+                    }}
+                    className="flex gap-[10px] items-center justify-center text-white bg-[var(--color-color-stroke)] w-[129px] h-[37px] rounded-md"
+                  >
                     <span className="text-md">Tuesday</span>
                     <img className="w-[12px] h-[18px]" src={dropdownIcon} />
                   </button>
+                  {isDaysOpen && (
+                    <ul className="absolute  w-full bg-[var(--color-bg-search-box)] rounded-lg p-2 border border-[var(--color-color-stroke)]">
+                      {days.map((day, index) => (
+                        <li
+                          key={index}
+                          onClick={() => {
+                            setDay(day);
+                            setIsDaysOpen(false);
+                          }}
+                          className="w-full p-3 hover:bg-[var(--color-color-stroke)] cursor-pointer text-[var(--color-text)] text-left rounded-lg"
+                        >
+                          {day}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
                 </div>
               </div>
               <div className="flex flex-col justify-between w-[336px] h-full">
